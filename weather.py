a998ad73550d7a6ee5f31eb123772172fa67ab3a
Saransh Thakur <53107943+Saranshthakur@users.noreply.github.com>
2026-04-21 19:59:46 -0500

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,12 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Weather Server")
+
+
+@mcp.tool()
+def get_weather(city: str) -> str:
+    return f"The weather in {city} is sunny."
+
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable_http", port=8000)
