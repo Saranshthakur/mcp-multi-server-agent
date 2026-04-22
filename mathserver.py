234e97136b695d8b97db8f1427098421a71ca58e
Saransh Thakur <53107943+Saranshthakur@users.noreply.github.com>
2026-04-21 19:59:14 -0500

    Create mathserver.py

---
+++ mathserver.py
@@ -0,0 +1,17 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Math Server")
+
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
